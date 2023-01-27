83f5d7216bbb25da7336ee48607942594aef000c
Arnav <arnavnayudu@ucsb.edu>
2023-01-27 12:36:11 -0800

    adding icons

--- hello_world/script.js
+++
@@ -1,8 +0,0 @@
-function alertSearch(string){
-    alert("Searching for "+string)
-}
-
-document.getElementById("searchButton").addEventListener("click", 
-    function(){
-        alertSearch(document.getElementById("query").value)
-    }, false);
