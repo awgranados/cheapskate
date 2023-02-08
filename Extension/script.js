8da3acde6700c3bfcfe3dc2f47c367f7c6c40429
Jake Yim <jakeyim@umail.ucsb.edu>
2023-02-07 18:35:56 -0800

    data is fetched from scraper and displayed on extension

--- Extension/script.js
+++ Extension/script.js
@@ -1,11 +1,28 @@
-function alertSearch(string){
-    alert("searching for "+string)
-}
-
 const form = document.getElementById("searchForm")
 
-form.addEventListener("submit", function(){
-    const val = document.getElementById("name").value
-    alertSearch(val)
-}, false)
+form.addEventListener("submit", function(e){
+    e.preventDefault();
 
+    const stringParam = document.getElementById("name").value
+    param = ""
+    words = stringParam.split(" ")
+    for (let i = 0; i < words.length; i++){
+        param = param.concat("", words[i])
+        if (i != words.length-1){
+            param = param.concat("+")
+        } 
+    }
+    console.log(param)
+
+    const site = "https://store.steampowered.com/search/?term="
+    const url = site.concat("", param)
+
+    console.log("fetching...")
+    fetch(`http://localhost:3000/scrape/${encodeURIComponent(url)}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("result").innerHTML = JSON.stringify(data, null, 2);
+    });
+
+}, false)
