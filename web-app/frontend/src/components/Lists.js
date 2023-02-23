e4bbbf360de2df31bbf89c38d915f2d63ab75754
Jay145 <41591950+Jay145@users.noreply.github.com>
2023-02-23 15:52:10 -0800

    Revert "Revert "Working Form Post""

--- web-app/frontend/src/components/Lists.js
+++ web-app/frontend/src/components/Lists.js
@@ -1,46 +1,81 @@
 import React, { useEffect, useState } from "react";
-// import {Link} from 'react-router-dom';
-
+import FormData from "form-data";
 
 function Lists() {
-    useEffect( () => {
+  const [listInput, setListInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const data = await fetch("/lists");
+    const items = await data.json();
+    setItems(items);
+  };
+
+  const handleListInputChange = (event) => {
+    setListInput(event.target.value);
+  };
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const response = await fetch("/addList", {
+      method: "POST",
+      body: formData,
+    });
+    try {
+      const data = await response.json();
+      if (response.ok) {
+        alert(data.message);
         fetchItems();
-    }, []);
+      } else {
+        alert(data.error);
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Error adding list");
+    }
+    setListInput("");
+};
 
-    const [items, setItems] = useState([]);
-
-    const fetchItems = async () => {
-        const data = await fetch('/lists');
-        const items = await data.json();
-        setItems(items);
-    };
-
-    return(
-        <section>
-            
-            <div class="container-fluid">
-                <h1 class="mt-5">Lists</h1>
-                <form method="POST" action="/addList">
-                    <div class="input-group justify-content-center">
-                        <div class="input-group-prepend">
-                            <input type="text" name="listInput" class="form-control" />
-                            <input type="submit" value="Add" class="btn btn-primary mb-2" />
-                        </div>
-                    </div>
-                </form>
-
-                {
-                items.map(item => (
-                    <div class="row padding">
-                        <div class="alert alert-info rounded-pill" role="alert">
-                            <i class="fa fa-user mr-2"></i> <i>{item.user.fullname} ({item.user.username}): {item.list}</i>
-                        </div>
-                    </div>       
-                ))
-                }
+  return (
+    <section>
+      <div className="container-fluid">
+        <h1 className="mt-5">Lists</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group justify-content-center">
+            <div className="input-group-prepend">
+              <input
+                type="text"
+                name="listInput"
+                className="form-control"
+                value={listInput}
+                onChange={handleListInputChange}
+              />
+              <input
+                type="submit"
+                value="Add"
+                className="btn btn-primary mb-2"
+              />
             </div>
-        </section>
-    );
+          </div>
+        </form>
+        {items.map((item) => (
+          <div className="row padding" key={item._id}>
+            <div className="alert alert-info rounded-pill" role="alert">
+              <i className="fa fa-user mr-2"></i>{" "}
+              <i>
+                {item.user.fullname} ({item.user.username}): {item.list}
+              </i>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Lists;
