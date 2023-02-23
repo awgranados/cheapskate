32011012c65ac0ad0371fee13c08b99cb6cdde12
Jin Jeong <wjeong@ucsb.edu>
2023-02-23 11:49:13 -0800

    Added a dropdown to the user profile on top right, users can now log out from the drop down instead of the navbar.

--- web-app/frontend/src/App.js
+++ web-app/frontend/src/App.js
@@ -25,7 +25,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/lists" element={<Lists />} />
             <Route path="/form" element={<FormTab />} />
-
         </Routes>
       </header>
     </div>
