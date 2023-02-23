0e0dfa1710290a95f7e7834e308a760ceb3efccc
Kelvin Zhang <bojunzhang1@gmail.com>
2023-02-23 15:35:43 -0800

    Working Form Post

--- web-app/backend/models/Schemas.js
+++ web-app/backend/models/Schemas.js
@@ -2,26 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type:String, required:true},
-    fullname: {type:String, required:true},
-    entryDate: {type:Date, default:Date.now}
+  username: { type: String, required: false },
+  fullname: { type: String, required: false },
+  entryDate: { type: Date, default: Date.now }
 });
 
 const listSchema = new Schema({
-    lists: {type:String, required:true},
-    user: {type:Schema.Types.ObjectId, ref:'users'}
+  list: { type: String, required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'users' }
 });
 
 const gameSchema = new Schema({
-    title: {type:String, required:true},
-    description: {type:String, required:true},
-    user: {type:Schema.Types.ObjectId, ref:'users'}
+  title: { type: String, required: true },
+  desc: { type: String, required: false },
+  user: { type: Schema.Types.ObjectId, ref: 'users' }
 });
 
 const Users = mongoose.model('users', userSchema, 'users');
 const Lists = mongoose.model('lists', listSchema, 'lists');
 const Games = mongoose.model('games', gameSchema, 'games');
-const mySchemas = {'Users': Users, 'Lists': Lists, 'Games': Games};
+const mySchemas = { 'Users': Users, 'Lists': Lists, 'Games': Games };
 
 module.exports = mySchemas;
-
