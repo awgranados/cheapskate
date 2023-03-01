1d654c8eebf7b0b36889462f584965ea52a1b1e5
Arnav <arnavnayudu@ucsb.edu>
2023-03-01 15:50:24 -0800

    created scrape endpoint

--- web-app/backend/routes/handler.js
+++ web-app/backend/routes/handler.js
@@ -1,22 +1,86 @@
 const express = require('express');
+const puppeteer = require('puppeteer')
 const router = express.Router();
-const formidable = require('formidable');
 const Schemas = require('../models/Schemas.js');
 const { Lists, Users } = require('../models/Schemas');
 
-router.get('/addUser', async(req, res) => {
-    const user = {username: 'user_two', fullname: 'User Two'};
-    const newUser = new Schemas.Users(user);
+async function scrapeProduct(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  function Game(name, price, link) {
+      this.name = name
+      this.price = price
+      this.link = link
+  }
 
-    try {
-        await newUser.save( async(err, newUserResult) => {
-            console.log('New user created!');
-            res.end('New user created!');
-        });
-    } catch (err) {
-        console.log(err);
-        res.end('User not added!');
-    }
+  var data = []
+  games = new Array();
+
+  for(let i = 1; i < 6; i++){
+      let num_text = i.toString();
+
+      x_path_title = '//*[@id="search_resultsRows"]/a[' + num_text + ']/div[2]/div[1]/span';
+      var [el] = await page.$x(x_path_title);
+      var get_name = await el.getProperty('textContent');
+      var name = await get_name.jsonValue(); 
+
+      x_path_price = '//*[@id="search_resultsRows"]/a[' + num_text + ']/div[2]/div[4]/div[2]';
+      var [el2] = await page.$x(x_path_price);
+      var get_price = await el2.getProperty('textContent');
+      var price = await get_price.jsonValue();
+      price = price.replace(/(\r\n|\n|\r|\t)/gm, "").trim();
+
+      x_path_link = '//*[@id="search_resultsRows"]/a[' + num_text + ']/@href';
+      var [el3] = await page.$x(x_path_link);
+      var get_link = await el3.getProperty('textContent');
+      var link = await get_link.jsonValue(); 
+
+      games.push(new Game(name, price, link))
+  }
+
+  for (i = 0; i < games.length; i++) {
+      var dict = {}
+      dict['name'] = games[i].name
+      dict['price'] = games[i].price
+      dict['link'] = games[i].link
+      data[i] = dict
+  }
+
+  browser.close();
+  return data
+}
+
+router.get('/scrape/:url', async (req, res, err) => {
+  // console.log(req)
+  const { url } = req.params;
+  console.log(url)
+
+  try{
+      data = await scrapeProduct(url)
+      console.log(data)
+      res.status(200).send(data);
+  }
+  catch{
+      console.log(err);
+      res.end();
+  }
+
+});
+
+router.get('/addUser', async(req, res) => {
+  const user = {username: 'user_two', fullname: 'User Two'};
+  const newUser = new Schemas.Users(user);
+
+  try {
+      await newUser.save( async(err, newUserResult) => {
+          console.log('New user created!');
+          res.end('New user created!');
+      });
+  } catch (err) {
+      console.log(err);
+      res.end('User not added!');
+  }
 });
 router.get('/lists', async (req, res) => {
   const lists = Schemas.Lists;
@@ -53,8 +117,10 @@ router.post('/addList', async (req, res) => {
 });
 
 router.post('/postForm', async (req, res) => {
+  console.log(req)
   const gameTitle = req.body.title;
   const gameDesc = req.body.description;
+  console.log(req.body)
   const user = Schemas.Users;
   const userId = await user.findOne({ username: 'clifford' }).exec();
 
