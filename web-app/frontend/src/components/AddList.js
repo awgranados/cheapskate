5d2e2af8b6893f2997ad906a17bc68e6b80402b8
Kelvin Zhang <bojunzhang1@gmail.com>
2023-03-17 23:27:54 -0700

    some final ui fixes

--- web-app/frontend/src/components/AddList.js
+++ web-app/frontend/src/components/AddList.js
@@ -40,7 +40,7 @@ function AddList() {
   return (
     <section>
       <div className="container-fluid">
-        <h1 className="mt-5">Lists</h1>
+        <h1 className="mt-5"></h1>
         <form onSubmit={handleSubmit}>
           <div className="input-group justify-content-center">
             <div className="input-group-prepend">
